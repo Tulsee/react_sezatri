0ed57273ef07b4645daf798d173db4fe174799ff
tulsee <ramtulsi022@gmail.com>
2021-10-29 15:53:47 +0545

    login successfully using email and password and get json token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,3 +11,31 @@ exports.registerUser = async (req, res, next) => {
   });
   sendToken(user, 200, res);
 };
+
+// login user => /api/v1/login
+
+exports.loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if email and password is enter by user or not
+  if (!email || !password) {
+    errors = 'Please enter email and password';
+    return res.status(404).json(errors);
+  }
+
+  // finding user in database
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user) {
+    errors = 'User not found';
+    return res.status(404).json(errors);
+  }
+  // check if password is match or not
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    errors = 'Invalid Password';
+    return res.status(404).json(errors);
+  }
+  sendToken(user, 200, res);
+};
